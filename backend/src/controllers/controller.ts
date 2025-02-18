3e5ec2b397c4aeed60f8bc593963b6823222c774
rafirafi03 <ahamedrafirafi03@gmail.com>
2025-02-18 11:26:13 +0530

    updated

--- backend/src/controllers/controller.ts
+++ backend/src/controllers/controller.ts
@@ -56,7 +56,7 @@ export const loginUser = async (
 
       res.cookie("userToken", token, {
         httpOnly: true, 
-        secure: process.env.NODE_ENV === "production", 
+        secure: false, 
         maxAge: 3600000, 
       });
 
