926a7fb987d1f3640de14264e58165b4f4a1ca88
rafirafi03 <ahamedrafirafi03@gmail.com>
2025-02-18 12:24:43 +0530

    updatedddd

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -10,7 +10,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: "https://travel-tracking.vercel.app",
+    origin: "*",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
