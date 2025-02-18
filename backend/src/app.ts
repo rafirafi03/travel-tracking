ba7d19cbd4b910fc4619dbac7e7ebe7ba4748c43
rafirafi03 <ahamedrafirafi03@gmail.com>
2025-02-18 12:00:36 +0530

    updateddd

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,24 +1,23 @@
 import express from "express";
 import cors from "cors";
-import routes from './routes/route'
-import cookieParser from 'cookie-parser';
+import routes from "./routes/route";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
 
-
 app.use(
   cors({
-    origin: "https://travel-tracking.vercel.app", 
-    credentials: true, 
-    methods: ["GET", "POST", "PUT", "DELETE"], 
-    allowedHeaders: ["Content-Type", "Authorization"], 
+    origin: "https://travel-tracking.vercel.app",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 
-app.use('/api', routes)
+app.use("/api", routes);
 
 // Test Route
 app.get("/", (req, res) => {
